4d39ef1dd874c05c4fc061fc584eab0f55f14611
정두콩 <junggg097@daum.net>
2024-02-06 15:06:05 +0900

    query tests prep

--- src/test/java/com/example/querydsl/QuerydslQueryTests.java
+++ src/test/java/com/example/querydsl/QuerydslQueryTests.java
@@ -15,22 +15,21 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.example.querydsl.entity.QItem.item;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
 @ActiveProfiles("test")
-public class QuerydslQTypeTests {
+public class QuerydslQueryTests {
     @Autowired
     private ItemRepository itemRepository;
     @Autowired
     private ShopRepository shopRepository;
     @Autowired
-    private JPAQueryFactory jpaQueryFactory;
+    private JPAQueryFactory queryFactory;
 
-    // @BeforeEach : 각 테스트 전에 실행할 코드를 작성하는 영역
+    // @BeforeEach: 각 테스트 전에 실행할 코드를 작성하는 영역
     @BeforeEach
     public void beforeEach() {
         Shop shopA = shopRepository.save(Shop.builder()
@@ -66,62 +65,19 @@ public class QuerydslQTypeTests {
                         .name("itemD")
                         .price(10000)
                         .stock(50)
+                        .build(),
+                Item.builder()
+                        .name("itemE")
+                        .price(11000)
+                        .stock(10)
+                        .build(),
+                Item.builder()
+                        .price(10500)
+                        .stock(25)
                         .build()
         ));
     }
 
-    @Test
-    public void qType() {
-        QItem qItem = new QItem("item");
-        Item found = jpaQueryFactory
-                .select(qItem)
-                .from(qItem)
-                .where(qItem.name.eq("itemA"))
-                .fetchOne();
-        assertEquals("itemA", found.getName());
 
 
-        found = jpaQueryFactory
-                // SELECT + FROM 절
-                .selectFrom(qItem)
-                .where(qItem.name.eq("itemB"))
-                .fetchOne();
-        assertEquals("itemB", found.getName());
-
-
-        // QItem 생성자의 인자가 Alias로 동작
-        QItem qItem2 = new QItem("item2");
-        found = jpaQueryFactory
-                .selectFrom(qItem2)
-                .where(qItem2.name.eq("itemC"))
-                .fetchOne();
-        assertEquals("itemC", found.getName());
-
-
-        // 함부로 섞어쓰면 예외 발생
-        assertThrows(Exception.class, () -> {
-            jpaQueryFactory
-                    // SELECT item FROM Item item
-                    .selectFrom(qItem)
-                    // WHERE item2.name = "itemD" -> ?????
-                    .where(qItem2.name.eq("itemD"))
-                    .fetchOne();
-        });
-        // 섞어서 써야되는 경우는 나 자신과의 연관관계
-        // 친구, 팔로우 User - ManyToMany - User
-
-        // 평소에는 기본 정적 QItem 인스턴스를 사용
-        found = jpaQueryFactory
-                .selectFrom(QItem.item)
-                .where(QItem.item.name.eq("itemA"))
-                .fetchOne();
-        assertEquals("itemA", found.getName());
-
-        // import static으로 바로 사용 가능
-        found = jpaQueryFactory
-                .selectFrom(item)
-                .where(item.name.eq("itemB"))
-                .fetchOne();
-        assertEquals("itemB", found.getName());
-    }
 }
